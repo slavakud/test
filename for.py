b6cc6d3e736aa779dc6ba4e201547d4eed9191b0
slavakud <slava-kudinow@yandex.ru>
2023-02-03 11:28:45 +0300

    My first commit

---
+++ for.py
@@ -0,0 +1,12 @@
+smile= '\U0001f600'
+print(type(smile))
+count=0
+for number in range(10):
+
+    while count== number:
+        count+=1
+        print(smile)
+        smile=smile+'\U0001f600'
+
+
+
