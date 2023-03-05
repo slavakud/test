e810f172382b8e5f4a50602e1595ea831b73456a
slavakud <124340431+slavakud@users.noreply.github.com>
2023-03-05 19:21:59 +0300

    класс CashCalculator
    Дописан класс CashCalculator
    исправлены ошибки в r1-r6

--- calculator/.idea/cash_and _callories.py
+++ calculator/.idea/cash_and _callories.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 class Calculator():
     record=[]
     def __init__(self,limit_cal,currency,rub, usd,eur):
@@ -6,22 +7,34 @@ class Calculator():
         self.usd=usd
         self.eur=eur
         self.limit_cal=limit_cal
-        CaloriesCalculator.get_calories_remaine(self)
+
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remaine(self):
         print
-        if self.limit_cal >= r1.amount+r2.amount+r3.amount:
-            N=self.limit_cal-(r1.amount+r2.amount+r3.amount)
+        if self.limit_cal >= r4.amount+r5.amount+r6.amount:
+            N=self.limit_cal-(r4.amount+r5.amount+r6.amount)
             print(f"Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {N} кКал")
         else:
             print(f"Хватит есть!, если лимит достигнут или превышен")
 
 
 
-# class CashCalculator(Calculator):
-#     def get_today_cash_remained(currency)
+class CashCalculator(Calculator):
+    def get_today_cash_remained(self,name):
+        cash= values.currency-(r1.amount+r2.amount+r3.amount)
+        if cash<= 0:
+            print("Лимит превышен")
+
+        if name=="rub":
+            print (f"можно купить на {cash}")
+
+        elif name=="eur":
+
+            print (f'можно купить на uer {round(cash/values.eur,2)}' )
+        else:
+            print (f'можно купить на usd {round(cash/values.usd,2)}')
 
 class Record():
     def __init__(self,amount,comment,date):
@@ -31,12 +44,14 @@ class Record():
 
 # для CashCalculator
 r1 = Record(amount=145, comment="Безудержный шопинг", date="08.03.2019")
-r2 = Record(amount=1560, comment="Наполнение потребительской корзины", date="09.03.2019")
+r2 = Record(amount=1060, comment="Наполнение потребительской корзины", date="09.03.2019")
 r3 = Record(amount=691, comment="Катание на такси", date="08.03.2019")
 
 # для CaloriesCalculator
 r4 = Record(amount=1186, comment="Кусок тортика. И ещё один.", date="24.02.2019")
 r5 = Record(amount=84, comment="Йогурт.", date="23.02.2019")
-r6 = Record(amount=1140, comment="Баночка чипсов.", date="24.02.2019")
-
-values=Calculator(2300,1000,1,75,85)
+r6 = Record(amount=140, comment="Баночка чипсов.", date="24.02.2019")
+values=Calculator(2300,10000,1,75,85)
+name= input("Введите валюту")
+CashCalculator.get_today_cash_remained(values,name)
+CaloriesCalculator.get_calories_remaine(values)
